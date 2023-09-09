3c8b26c13c4423b04db97249c90ea4887a4526d6
Yinan Zheng <y-zheng@northwestern.edu>
2023-09-08 23:11:01 -0500

    HIMA 2.2.1
    Add de-biased lasso HIMA and quantile HIMA

--- R/HIMA-package.R
+++ R/HIMA-package.R
@@ -27,23 +27,25 @@
 #' 
 #' Maintainer: Yinan Zheng \email{y-zheng@@northwestern.edu}
 #' 
-#' @references 1. Zhang H, Zheng Y, Zhang Z, Gao T, Joyce B, Yoon G, Zhang W, Schwartz J, 
-#' Just A, Colicino E, Vokonas P, Zhao L, Lv J, Baccarelli A, Hou L, Liu L. Estimating and 
-#' Testing High-dimensional Mediation Effects in Epigenetic Studies. Bioinformatics. 
-#' 2016;32(20):3150-4. DOI: 10.1093/bioinformatics/btw351. PubMed PMID: 27357171; 
-#' PMCID: PMC5048064.
+#' @references 1. Zhang H, Zheng Y, Zhang Z, Gao T, Joyce B, Yoon G, Zhang W, Schwartz J, Just A, Colicino E, Vokonas P, Zhao L, 
+#' Lv J, Baccarelli A, Hou L, Liu L. Estimating and Testing High-dimensional Mediation Effects in Epigenetic Studies. 
+#' Bioinformatics. 2016. DOI: 10.1093/bioinformatics/btw351. PMID: 27357171. PMCID: PMC5048064
 #' 
 #' 2. Perera C, Zhang H, Zheng Y, Hou L, Qu A, Zheng C, Xie K, Liu L. 
 #' HIMA2: high-dimensional mediation analysis and its application in epigenome-wide DNA methylation data. 
-#' BMC Bioinformatics. 2022. DOI: 10.1186/s12859-022-04748-1. PMID: 35879655. PMCID: PMC9310002.
+#' BMC Bioinformatics. 2022. DOI: 10.1186/s12859-022-04748-1. PMID: 35879655. PMCID: PMC9310002
 #' 
-#' 3. Zhang H, Zheng Y, Hou L, Liu L. Mediation Analysis for Survival Data with High-Dimensional 
-#' Mediators. Bioinformatics. 2021;37(21):3815-21. DOI: 10.1093/bioinformatics/btab564. 
-#' PubMed PMID: 34343267; PMCID: PMC8570823.
+#' 3. Zhang H, Zheng Y, Hou L, Zheng C, Liu L. Mediation Analysis for Survival Data with High-Dimensional Mediators. 
+#' Bioinformatics. 2021. DOI: 10.1093/bioinformatics/btab564. PMID: 34343267. PMCID: PMC8570823
 #' 
-#' 4. Zhang H, Chen J, Feng Y, Wang C, Li H, Liu L. Mediation effect selection in high-dimensional 
-#' and compositional microbiome data. Stat Med. 2021;40(4):885-96. DOI: 10.1002/sim.8808. 
-#' PubMed PMID: 33205470; PMCID: PMC7855955.
+#' 4. Zhang H, Chen J, Feng Y, Wang C, Li H, Liu L. Mediation effect selection in high-dimensional and compositional microbiome data. 
+#' Stat Med. 2021. DOI: 10.1002/sim.8808. PMID: 33205470; PMCID: PMC7855955
+#' 
+#' 5. Zhang H, Chen J, Li Z, Liu L. Testing for mediation effect with application to human microbiome data. 
+#' Stat Biosci. 2021. DOI: 10.1007/s12561-019-09253-3. PMID: 34093887; PMCID: PMC8177450
+#' 
+#' 6. Zhang H, Hong X, Zheng Y, Hou L, Zheng C, Wang X, Liu L. High-Dimensional Quantile Mediation Analysis with Application to a Birth 
+#' Cohort Study of Mother–Newborn Pairs. Bioinformatics. 2023. (In press)
 #' 
 #' @keywords package
 #' 
