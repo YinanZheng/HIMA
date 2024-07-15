d8b04002b3ac83dbd29dea58d118bab7b946ec6d
Yinan Zheng <y-zheng@northwestern.edu>
2024-07-15 00:38:25 -0500

    HIMA 2.2.2 Working Version

--- R/hima.R
+++ R/hima.R
@@ -102,19 +102,13 @@ hima <- function(X, Y, M, COV.XM = NULL, COV.MY = COV.XM,
     {
       X <- scale(X)
       M <- scale(M)
-      if(!is.null(COV.XM))
-      {
-        COV.XM <- scale(COV.XM)
-        COV.MY <- scale(COV.MY)
-      }
+      if(!is.null(COV.XM)) COV.XM <- scale(COV.XM)
+      if(!is.null(COV.MY)) COV.MY <- scale(COV.MY)
     } else {
       X <- as.matrix(X)
       M <- as.matrix(M)
-      if(!is.null(COV.XM))
-      {
-        COV.XM <- as.matrix(COV.XM)
-        COV.MY <- as.matrix(COV.MY)
-      }
+      if(!is.null(COV.XM)) COV.XM <- as.matrix(COV.XM)
+      if(!is.null(COV.MY)) COV.MY <- as.matrix(COV.MY)
     }
  
     if(is.null(topN)) {
