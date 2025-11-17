d8510006f220774a10ba7047dafa84dd4b829d43
Yinan Zheng <y-zheng@northwestern.edu>
2025-11-17 00:18:17 -0600

    Longitudinal Survival HIMA (v2.3.3)

--- R/HIMA-package.R
+++ R/HIMA-package.R
@@ -7,7 +7,7 @@
 #' HIMA can also handle survival data mediation analysis and perform quantile mediation analysis.
 #'
 #' \tabular{ll}{ Package: \tab HIMA\cr Type: \tab Package\cr
-#' Version: \tab 2.3.3\cr Date: \tab 2025-11-16\cr License: \tab GPL-3\cr }
+#' Version: \tab 2.3.3\cr Date: \tab 2025-11-17\cr License: \tab GPL-3\cr }
 #'
 #' @name HIMA-package
 #'
